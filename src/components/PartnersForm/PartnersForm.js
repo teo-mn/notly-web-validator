7401445eba91f7c21ac22d0bdfb890243986e1c9
Boldmaa Work <s.boldmaa@gmail.com>
2022-10-10 16:43:12 +0800

    Registration form link updated

--- src/components/PartnersForm/PartnersForm.js
+++ src/components/PartnersForm/PartnersForm.js
@@ -4,7 +4,7 @@ const PartnersForm = () => {
 
   return (
       <div className={'bg-main-bg'}>
-        <iframe id={'partnersForm'} className={'h-full w-screen'} src="https://forms.office.com/r/q6g1fR6VKn" title={'Бүртгүүлэх'}/>
+        <iframe id={'partnersForm'} className={'h-full w-screen'} src="https://forms.office.com/pages/responsepage.aspx?id=dFXv6mMFTUafGDiCX7kDF1MgBq7wu0pNm24Ii6vAlllUMUZDRTMxNTFaQVY1WDFMV0JaTEJNTzlaRS4u" title={'Бүртгүүлэх'}/>
       </div>
   )
 
